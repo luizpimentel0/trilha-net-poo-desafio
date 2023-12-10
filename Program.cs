99fbf1f428524d0bcc4dd82efaa223643224aa18
Luiz Fernando <luiz.fpimentel0@gmail.com>
2023-12-10 15:52:00 -0300

    feat: Instanciando classes Iphone e Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "11993322933", modelo: "C12", imei: "111223445", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Smartphone iphone = new Iphone(numero: "11922334455", modelo: "14 plus", imei: "223111223", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
